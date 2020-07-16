48ee89a3be305cd0c101f39b0fa933df320214ed
mi <g.mi.hira+github@gmail.com>
2020-07-16 11:20:46 +0900

    initial commit

---
+++ main.js
@@ -0,0 +1,24 @@
+fetch('https://www.data.jma.go.jp/fcd/yoho/wdist/jp/data/wdist/VPFD/130010.json')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    // console.log(JSON.stringify(myJson));
+    main = document.getElementById("main");
+    period = myJson.areaTimeSeries;
+    point = myJson.pointTimeSeries;
+    for( key in myJson.areaTimeSeries.timeDefines){
+        var newDiv = document.createElement("div");
+        var newContent = document.createTextNode(
+            `${moment(period.timeDefines[key].dateTime).format('MM/DD hh:mm')}  ${period.weather[key]} ${point.temperature[key]}℃ ${period.wind[key].direction} ${period.wind[key].speed}m/s `
+        );
+        newDiv.appendChild(newContent);
+        document.body.insertBefore(newDiv,main);
+        console.log(myJson.areaTimeSeries.timeDefines[key].dateTime);
+        console.log(myJson.areaTimeSeries.weather[key]);
+        console.log(myJson.areaTimeSeries.wind[key].direction);
+
+    }
+    
+
+  });
